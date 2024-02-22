64df5d919bc2d024dfa7aa62089f327f40aa00d0
Satyam <160597502+satyamchaudhary1@users.noreply.github.com>
2024-02-22 21:00:13 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let accountEmail ="satyamchaudhary6399gmail.com";
+var accountName = "satyam";
+accountCity = "kasganj"
+
+console.table([accountId , accountEmail , accountName , accountCity]);
+
